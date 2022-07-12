f56d5c3c920a35dcf75ad390afabb33aa7ffdc67
jbalasis <cs2180008@di.uoa.gr>
2022-07-12 17:31:12 +0300

    changes for catalogue-ui compatibility

--- pages/landingpages/datasets/dataset-landing-page.component.ts
+++ pages/landingpages/datasets/dataset-landing-page.component.ts
@@ -1,6 +1,11 @@
 import {Component} from "@angular/core";
 import {LandingPageService} from "../../../catalogue-ui/services/landing-page.service";
 import {LandingPageComponent} from "../../../catalogue-ui/pages/landingpages/dataset/landing-page.component";
+import {environment} from "../../../../environments/environment";
+import {ActivatedRoute, Router} from "@angular/router";
+import {NavigationService} from "../../../services/navigation.service";
+import {UserService} from "../../../services/user.service";
+import {UserInfo} from "../../../domain/userInfo";
 
 declare var UIkit: any;
 
@@ -11,6 +16,37 @@ declare var UIkit: any;
 })
 
 export class DatasetLandingPageComponent extends LandingPageComponent {
+
+  userInfo: UserInfo = null;
+  projectName: string = null;
+
+  constructor(protected override route: ActivatedRoute,
+              protected override landingPageService: LandingPageService,
+              protected navigationService: NavigationService,
+              protected router: Router,
+              protected userService: UserService) {
+    super(route, landingPageService)
+  }
+
+  override ngOnInit() {
+    super.ngOnInit();
+    this.projectName = environment.projectName;
+    if (this.userService.userInfo !== null) {
+      this.userInfo = this.userService.userInfo;
+    } else {
+      this.subscriptions.push(
+        this.userService.getUserInfo().subscribe(
+          res => {
+            this.userInfo = res;
+            this.userService.roleToSessionStorage(res);
+          },
+          error => {
+            console.log(error);
+          }
+        )
+      );
+    }
+  }
 
   gotoRequestData(instanceVersion, datasetId) {
     this.navigationService.setDataRequestIds(instanceVersion, datasetId);
