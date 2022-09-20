0b361faa6e72845718b3186940476192d9850322
jbalasis <cs2180008@di.uoa.gr>
2022-09-20 11:22:37 +0300

    git subrepo pull src/dataSpaceUI
    subrepo:
      subdir:   "src/dataSpaceUI"
      merged:   "3cbdb6f"
    upstream:
      origin:   "git@github.com:madgeek-arc/dataSpaceUI.git"
      branch:   "main"
      commit:   "3cbdb6f"
    git-subrepo:
      version:  "0.4.3"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "2f68596"

--- app/pages/search/resource-search/resource-search.component.ts
+++ app/pages/search/resource-search/resource-search.component.ts
@@ -1,6 +1,10 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, ElementRef, OnInit, ViewChild} from "@angular/core";
 import {ResourcePayloadService} from "../../../services/resource-payload.service";
 import {Paging} from "../../../../catalogue-ui/domain/paging";
+import {ActivatedRoute, Router} from "@angular/router";
+import {URLParameter} from "../../../../catalogue-ui/domain/url-parameter";
+import {debounceTime, distinctUntilChanged, filter, fromEvent} from "rxjs";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-tool-search',
@@ -10,51 +14,114 @@ import {Paging} from "../../../../catalogue-ui/domain/paging";
 
 export class ResourceSearchComponent implements OnInit {
 
+  @ViewChild('searchInput', { static: true }) searchInput: ElementRef;
+
+  resourceType: string = null;
   payload: Paging<any> = null
+  urlParameters: {key: string, values: string[]}[] = [];
 
   pages: number[] = [];
   pageTotal: number = 0;
-  pageSize: number = 0;
+  pageSize: number = 10;
   currentPage: number = 0;
 
-  constructor(private resourcePayloadService: ResourcePayloadService) {
-  }
+  searchQuery: string = null;
+
+  constructor(private resourcePayloadService: ResourcePayloadService, private route: ActivatedRoute,
+              private router: Router) {}
 
   ngOnInit() {
-    this.resourcePayloadService.getItemsByResourceType('tool').subscribe(
-      next => {
-        this.payload = next;
-        this.pageSize = next.to - next.from;
-        this.pageTotal = Math.ceil(next.total/this.pageSize);
-        this.pages = Array(this.pageTotal).fill(0).map((x,i)=>i);
+    this.route.params.subscribe(params => {
+      this.resourceType = params['resourceType'];
+      this.route.queryParams.subscribe(params => {
+        for (const [key, value] of Object.entries(params)) {
+          if (key === 'query') {
+            this.searchQuery = value;
+          }
+        }
+        this.resourcePayloadService.getItemsWithQueryParams(this.resourceType, params).subscribe(
+          next => {
+            this.payload = next;
+            this.initPagination(this.payload.from, this.payload.total);
+          },
+          error => {console.log(error)},
+          () => {}
+        )
+      })
+    });
+
+    fromEvent(this.searchInput.nativeElement, 'keyup').pipe(
+      map((event: any) => { // get value
+        return event.target.value;
+      })
+      // , filter(res => res.length > 2) // if character length greater then 2
+      , debounceTime(500) // Time in milliseconds between key events
+      , distinctUntilChanged() // If previous query is diffent from current
+      ).subscribe((text: string) => {
+        this.updateURLParameters('query', text);
+        this.navigateUsingParameters();
       }
-    )
+    );
+
+  }
+
+  updateURLParameters(key, value) {
+    for (const urlParameter of this.urlParameters) {
+      if (urlParameter.key === key) {
+        urlParameter.values = [value];
+        return;
+      }
+    }
+    this.urlParameters.push({key: key, values: [value]});
+  }
+
+  navigateUsingParameters() {
+    const map: { [name: string]: string; } = {};
+    for (const urlParameter of this.urlParameters) {
+      map[urlParameter.key] = urlParameter.values.join(',');
+    }
+    return this.router.navigate(['/search/tool'], {queryParams: map});
+  }
+
+  initPagination(from, total) {
+    this.currentPage = from / this.pageSize;
+    this.pageTotal = Math.ceil(total/this.pageSize);
+    this.pages = Array(this.pageTotal).fill(1).map((x,i)=>i);
   }
 
   goToPage(page: number) {
     this.currentPage = page;
-    // let from: number = (this.currentPage - 1) * this.pageSize
-    // this.updatePagingURLParameters(from);
-    // return this.navigateUsingParameters();
+    let from: number = (this.currentPage) * this.pageSize
+    this.updateURLParameters('from', from);
+    return this.navigateUsingParameters();
   }
 
   previousPage() {
-    if (this.currentPage > 1) {
+    if (this.currentPage > 0) {
       this.currentPage--;
-      // let from: number = this.searchResults.from;
-      // from -= this.pageSize;
-      // this.updatePagingURLParameters(from);
-      // this.navigateUsingParameters();
+      let from: number = (this.currentPage) * this.pageSize;
+      this.updateURLParameters('from', from);
+      this.navigateUsingParameters();
     }
   }
 
   nextPage() {
-    if (this.currentPage < this.pageTotal) {
+    if (this.currentPage < this.pageTotal-1) {
       this.currentPage++;
-      // let from: number = this.searchResults.from;
-      // from += this.pageSize;
-      // this.updatePagingURLParameters(from);
-      // this.navigateUsingParameters();
+      let from: number = (this.currentPage) * this.pageSize;
+      this.updateURLParameters('from', from);
+      this.navigateUsingParameters();
+    }
+  }
+
+  searchOnEnter(e) {
+    if(e.key === 'Enter') {
+      // returning false will prevent the event from bubbling up.
+      this.updateURLParameters('query', this.searchQuery);
+      this.navigateUsingParameters();
+      return false;
+    } else{
+      return true;
     }
   }
 
