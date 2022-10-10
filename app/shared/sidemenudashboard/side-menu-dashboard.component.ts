4924641960ecca3febe53930b13d324d7d3266ef
TheQuaker <cs2180008@di.uoa.gr>
2022-10-10 17:09:12 +0300

    removed logs

--- app/shared/sidemenudashboard/side-menu-dashboard.component.ts
+++ app/shared/sidemenudashboard/side-menu-dashboard.component.ts
@@ -42,8 +42,8 @@ export class SideMenuDashboardComponent implements OnInit, OnDestroy {
   }
 
   hasRole(role: string) {
-    console.log(role);
-    console.log(this.userInfo.roles.indexOf(role) > -1);
+    // console.log(role);
+    // console.log(this.userInfo.roles.indexOf(role) > -1);
     return this.userInfo.roles.indexOf(role) > -1;
     // return this.roles.indexOf(role) > -1;
   }
